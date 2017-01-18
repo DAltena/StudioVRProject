e6055eb14fbe8ba426aae102faf454e4313bec22
daltena <daltena@deloitte.com>
2017-01-18 14:04:25 -0500

    setup commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+	public bool walking;
+	// Use this for initialization
+	void Start () {
+		walking = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown ("space")) {
+			walking = !walking;
+		}
+
+		if (walking) {
+			transform.position = transform.position + Camera.main.transform.forward * 1.5f * Time.deltaTime;
+		}
+	}
+}
